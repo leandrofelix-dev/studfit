2811a0ec28b0770882d445d011866f218d7d8e70
Leandro Felix <leandro.felix@g4flex.com.br>
2024-10-13 16:44:26 -0300

    feat: aprimorar estilo do item da sidebar com novas classes de estilo

--- components/sidebar/sidebar-item.tsx
+++ components/sidebar/sidebar-item.tsx
@@ -1,6 +1,5 @@
 import NextLink from "next/link";
 import React from "react";
-import { useSidebarContext } from "../layout/layout-context";
 import clsx from "clsx";
 
 interface Props {
@@ -19,21 +18,32 @@ export const SidebarItem = ({ icon, title, isActive, href = "" }: Props) => {
     >
       <div
         className={clsx(
-          clsx(
-            "flex gap-2 w-full min-h-[44px] h-full items-center px-3.5 rounded-xl cursor-pointer transition-all duration-150 active:scale-[0.98]",
-            title !== "Frequência"
-              ? "[&_svg_path]:fill-success-500"
-              : "[&_svg_path]:fill-success-50"
-          ),
-          isActive
-            ? "bg-success-100 [&_svg_path]:fill-success-500"
-            : "hover:bg-default-100 [&_svg_path]:fill-success-50"
+          "flex gap-2 w-full min-h-[44px] h-full items-center px-3.5 rounded-xl cursor-pointer transition-all duration-150 active:scale-[0.98]",
+          isActive ? "bg-white/50" : "hover:bg-white/20"
         )}
       >
-        {icon}
+        <div
+          className={clsx(
+            title === "Frequência" && isActive
+              ? "[&_svg_path]:fill-success-500"
+              : title === "Frequência"
+                ? "[&_svg_path]:fill-default-50"
+                : isActive
+                  ? "[&_svg_path]:fill-default-900"
+                  : "[&_svg_path]:fill-default-50"
+          )}
+        >
+          {icon}
+        </div>
         <span
           className={clsx(
-            title === "Frequência" ? "text-success-50" : "text-default-900"
+            title === "Frequência" && isActive
+              ? "text-default-900"
+              : title === "Frequência"
+                ? "text-default-50"
+                : isActive
+                  ? "text-default-900"
+                  : "text-default-50"
           )}
         >
           {title}
